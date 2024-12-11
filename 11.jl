fdbdcc8de0868a2121d7dec0e4aefc0dbae4b97b
david.baker <davvidbaker@gmail.com>
2024-12-11 16:22:52 -0700

    day 11

--- 11.jl
+++ 11.jl
@@ -1,10 +1,8 @@
 using Test
+using Memoization
+
 test_input = "125 17"
 my_input = "70949 6183 4 3825336 613971 0 15 182"
-
-JUMP = 5
-cache = Dict()
-
 
 function evendigits(x)
     return length(string(x)) % 2 == 0
@@ -17,91 +15,46 @@ function splitstone(x)
 end
 
 
-function blink(stone, depth=1)
-    # if stone in cache[stone] 
+function blinkval(stone)
     if stone == 0
         return [1]
-        # push!(newstones, 1)
     elseif evendigits(stone)
         ss = splitstone(stone)
         return ss
     else
-        return [stone * 2024]
+        return [2024 * stone]
     end
 end
 
-
-function fifteen_later(digit)
-    dig_iterations = [[digit]]
-    for i ∈ 1:5
-        laststones = dig_iterations[end]
-        newstones = []
-        for stone ∈ laststones
-            push!(newstones, blink(stone)...)
-        end
-        push!(dig_iterations, newstones)
-    end
-
-    return dig_iterations[end]
-end
 
 function problem(input, iterations)
-
-
     input = parse.(Int, split(input, " "))
 
-    println("input: ", input)
-    stoners = [input]
-
-    # 0
-    # 1
-    # 2024
-    # 20 24
-    # 2 0 2 4
-    # 4048 1 4048 8096
-    # 40 48 2024 40 48 80 96
-    # 4 0 4 8 20 24 4 0 4 8 8 0 9 6
-    # 8096 1 8096 16192 2 0 2 4 ....
-
-    # 1 1 1 2 4 4 
-
-    fifteen_map = Dict()
-    for digit ∈ 1:9
-        fifteen_map[digit] = fifteen_later(digit)
+    total = 0
+    for stone in input
+        result = fun(stone, iterations)
+        total += result
     end
+    println("total: ", total)
 
-    for i ∈ 1:iterations
-        println(i, " ", length(fifteen_map))
-        # display(fifteen_map)
-        laststones = stoners[end]
-        newstones = []
-        println("length(laststones): ", length(laststones))
-        for stone ∈ laststones
-            if !(stone in keys(fifteen_map))
-                # println("stone: ", stone)
-                fifteen_map[stone] = fifteen_later(stone)
-            end
-            push!(newstones, fifteen_map[stone]...)
-            # println("done pushing")
-            # push!(newstones, fifteen_map[stone])
-            #     fifteen_map[stone] = fifteen_later(stone)
-            #     push
-
-
-            # push!(newstones, blink(stone)...)
-        end
-        # println("stoners: ", stoners)
-
-        # println("newstones: ", newstones)
-        push!(stoners, newstones)
-    end
-    # println("stoners: ")
-    # display(stoners)
-    return length(stoners[end])
+    return total
 end
 
-problem(test_input, Int(25 / JUMP))
-problem("1", 3)
-# @test problem(test_input, 25) == 55312
-@test problem(my_input, 25 / JUMP) == 185205
-# @test problem(my_input, 75 / 25) == 185205
+@memoize function fun(stone::Int, iter_remaining::Int)
+    bv = blinkval(stone)
+    if iter_remaining == 1
+        return length(bv)
+    else
+        output = 0
+        for v in bv
+            a = fun(v, iter_remaining - 1)
+            output += a
+        end
+    end
+
+
+    return output
+end
+@test problem(test_input, 25) == 55312
+@test problem(my_input, 25) == 185205
+@test problem(my_input, 75) == 221280540398419
